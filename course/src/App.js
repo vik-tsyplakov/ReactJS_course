a8c58dacc9dbc6d8978c45de38fbf490a6544811
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2023-01-12 19:51:12 +0400

    added-style

--- course/src/App.js
+++ course/src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
 import Counter from "./components/Counter";
+import "./styles/App.css";
+
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <div className="post">
+        <div className="post__content">
+          <strong> 1. JS</strong>
+          <div>JS - programming language</div>
+        </div>
+        <div className="post__btn">
+          <button>delete</button>
+        </div>
+      </div>
     </div>
   );
 }
