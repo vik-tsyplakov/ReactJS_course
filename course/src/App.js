833cd7911c4595a9ceb6c63a07844496a1ee7cfa
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2023-01-12 19:56:44 +0400

    created-PostItem-component

--- course/src/App.js
+++ course/src/App.js
@@ -1,19 +1,12 @@
 import React from "react";
 import Counter from "./components/Counter";
+import PostItem from "./components/PostItem";
 import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="post">
-        <div className="post__content">
-          <strong> 1. JS</strong>
-          <div>JS - programming language</div>
-        </div>
-        <div className="post__btn">
-          <button>delete</button>
-        </div>
-      </div>
+      <PostItem />
     </div>
   );
 }
---
+++ course/src/components/PostItem.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function PostItem() {
+  return (
+    <div>
+      <div className="post">
+        <div className="post__content">
+          <strong> 1. JS</strong>
+          <div>JS - programming language</div>
+        </div>
+        <div className="post__btn">
+          <button>delete</button>
+        </div>
+      </div>
+    </div>
+  );
+}
