209a701978b5b1de4b16476e09d8e4be1de9dfc9
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2023-01-12 22:43:40 +0400

    refactoring-button

--- course/src/components/UI/button/MyButton.jsx
+++ course/src/components/UI/button/MyButton.jsx
@@ -2,5 +2,9 @@ import React from "react";
 import classes from "./MyButton.module.css";
 
 export default function MyButton({ children, ...props }) {
-  return <button className={classes.myBtn}>{children}</button>;
+  return (
+    <button {...props} className={classes.myBtn}>
+      {children}
+    </button>
+  );
 }
