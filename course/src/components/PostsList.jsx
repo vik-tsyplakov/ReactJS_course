9aa9360bf0528197b47fbcabcd33478a97cca868
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2023-01-13 15:33:07 +0400

    fixed-correct-numbered-order-of-PostItems

--- course/src/components/PostsList.jsx
+++ course/src/components/PostsList.jsx
@@ -4,8 +4,8 @@ import PostItem from "./PostItem";
 export default function PostsList(props) {
   return (
     <div>
-      {props.posts.map((post) => (
-        <PostItem post={post} key={post.id} />
+      {props.posts.map((post, index) => (
+        <PostItem number={index + 1} post={post} key={post.id} />
       ))}
     </div>
   );
