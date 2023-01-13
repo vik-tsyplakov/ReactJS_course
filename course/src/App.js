39ea0c5a01093720e4be31fea6ed6a269f369c23
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2023-01-13 14:23:31 +0400

    created-MyInput-component

--- course/src/App.js
+++ course/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Counter from "./components/Counter";
 import PostsList from "./components/PostsList";
 import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 import "./styles/App.css";
 
@@ -13,13 +14,25 @@ function App() {
     { id: 4, title: "Python", description: "Python is programming language" },
     { id: 5, title: "PHP", description: "PHP is programming language" },
   ]);
+  const [title, setTitle] = useState("");
+  const bodyInputRef = useRef();
+  const addNewPost = (e) => {
+    e.preventDefault();
+    console.log(title);
+    console.log(bodyInputRef.current.value);
+  };
 
   return (
     <div className="App">
       <form>
-        <input type="text" placeholder="Post name" />
-        <input type="text" placeholder="Description" />
-        <MyButton>Create post</MyButton>
+        <MyInput
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          placeholder="Post name"
+        />
+        <MyInput ref={bodyInputRef} type="text" placeholder="Description" />
+        <MyButton onClick={addNewPost}>Create post</MyButton>
       </form>
       <PostsList posts={posts} />
     </div>
