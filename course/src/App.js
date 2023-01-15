51f96a5b4f6bc8f0ed600a455ef6dfb484c3b3ea
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2023-01-15 15:48:47 +0400

    added-sorting-by-name-and-description

--- course/src/App.js
+++ course/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Counter from "./components/Counter";
 import PostForm from "./components/PostForm";
 import PostsList from "./components/PostsList";
+import MySelect from "./components/UI/select/MySelect";
 
 import "./styles/App.css";
 
@@ -22,9 +23,26 @@ function App() {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
 
+  const [selectedSort, setSelectedSort] = useState("");
+
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+    setPosts([...posts.sort((a, b) => a[sort].localeCompare(b[sort]))]);
+  };
+
   return (
     <div className="App">
       <PostForm create={createPost} />
+      <hr style={{ marginTop: "10px", marginBottom: "15px" }} />
+      <MySelect
+        value={selectedSort}
+        onChange={sortPosts}
+        defaultValue="Sort by"
+        options={[
+          { value: "title", name: "By name" },
+          { value: "description", name: "By description" },
+        ]}
+      />
       {posts.length !== 0 ? (
         <PostsList remove={removePost} posts={posts} />
       ) : (
