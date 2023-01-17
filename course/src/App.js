e8b2bee8aa4d5ac63d2bb97768215be8d76bcaf2
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2023-01-17 13:49:38 +0400

    added-logic-to-addPost-button

--- course/src/App.js
+++ course/src/App.js
@@ -3,6 +3,7 @@ import Counter from "./components/Counter";
 import PostFilter from "./components/PostFilter";
 import PostForm from "./components/PostForm";
 import PostsList from "./components/PostsList";
+import MyButton from "./components/UI/button/MyButton";
 import MyModal from "./components/UI/MyModal/MyModal";
 import "./styles/App.css";
 
@@ -15,6 +16,7 @@ function App() {
     { id: 5, title: "PHP", description: "PHP is programming language" },
   ]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
+  const [modal, setModal] = useState(false);
 
   const sortedPosts = useMemo(() => {
     if (filter.sort) {
@@ -35,6 +37,7 @@ function App() {
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
+    setModal(false);
   };
 
   const removePost = (post) => {
@@ -43,10 +46,15 @@ function App() {
 
   return (
     <div className="App">
-      <MyModal>
+      <MyButton
+        style={{ backgroundColor: "#00DC01", marginTop: "15px" }}
+        onClick={() => setModal(true)}
+      >
+        Add post
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
         <PostForm create={createPost} />
       </MyModal>
-
       <hr style={{ marginTop: "10px", marginBottom: "15px" }} />
       <PostFilter filter={filter} setFilter={setFilter} />
       <PostsList remove={removePost} posts={sortedAndSearchedPosts} />
--- course/src/components/UI/MyModal/MyModal.jsx
+++ course/src/components/UI/MyModal/MyModal.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import cl from "./MyModal.module.css";
 
-export default function MyModal({ children }) {
+export default function MyModal({ children, visible, setVisible }) {
+  const rootClasses = [cl.myModal];
+  if (visible) {
+    rootClasses.push(cl.active);
+  }
+
   return (
-    <div className={[cl.myModal, cl.active].join(" ")}>
+    <div className={rootClasses.join(" ")}>
       <div className={cl.myModalContent}>{children}</div>
     </div>
   );
