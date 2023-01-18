a22300ec5dceb28df2b9ad6ad9695c41b299241c
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2023-01-18 17:47:31 +0400

    prepared-for-pagination

--- course/src/App.js
+++ course/src/App.js
@@ -10,6 +10,7 @@ import Loader from "./components/UI/loader/Loader";
 import MyModal from "./components/UI/MyModal/MyModal";
 
 import "./styles/App.css";
+import { getPageCount } from "./utils/pages";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -22,9 +23,20 @@ function App() {
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
+  let pagesArray = [];
+  for (let i = 0; i < totalPages; i++) {
+    pagesArray.push(i + 1);
+  }
+  console.log(pagesArray);
+
   const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-    const posts = await PostService.getAll();
-    setPosts(posts);
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data);
+    const totalCount = response.headers["x-total-count"];
+    setTotalPages(getPageCount(totalCount, limit));
   });
 
   useEffect(() => {
