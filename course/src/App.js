f67be852093917848b9e534e92ddf26a58cdfc8e
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2023-01-18 17:19:25 +0400

    creating-custom-hook-useFetching

--- course/src/App.js
+++ course/src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import PostService from "./API/PostService";
+import { useFetching } from "./components/Hooks/useFetching";
 import { usePosts } from "./components/Hooks/usePosts";
 import PostFilter from "./components/PostFilter";
 import PostForm from "./components/PostForm";
@@ -21,7 +22,10 @@ function App() {
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [isPostsLoading, setIsPostsLoading] = useState(false);
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll();
+    setPosts(posts);
+  });
 
   useEffect(() => {
     fetchPosts();
@@ -31,13 +35,6 @@ function App() {
     setPosts([...posts, newPost]);
     setModal(false);
   };
-
-  async function fetchPosts() {
-    setIsPostsLoading(true);
-    const posts = await PostService.getAll();
-    setPosts(posts);
-    setIsPostsLoading(false);
-  }
 
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
@@ -56,6 +53,7 @@ function App() {
       </MyModal>
       <hr style={{ marginTop: "10px", marginBottom: "15px" }} />
       <PostFilter filter={filter} setFilter={setFilter} />
+      {postError && <h2>An error has occurred: {postError}</h2>}
       {isPostsLoading ? (
         <div
           style={{
