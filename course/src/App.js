4ec4537be4777d31cb967afe9270ffdd6e91a974
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2023-01-18 15:27:36 +0400

    got-data

--- course/src/App.js
+++ course/src/App.js
@@ -29,7 +29,7 @@ function App() {
     const response = await axios.get(
       "https://jsonplaceholder.typicode.com/posts"
     );
-    console.log(response.data);
+    setPosts(response.data);
   }
 
   const removePost = (post) => {
