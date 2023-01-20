76cda5cce922e904bbd58ce822f2a6550716daa7
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2023-01-20 23:09:23 +0400

    added-styling-to-active-NavLink

--- course/src/components/UI/navbar/Navbar.jsx
+++ course/src/components/UI/navbar/Navbar.jsx
@@ -2,11 +2,24 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
+  let activeStyle = {
+    textDecoration: "underline",
+  };
   return (
     <div className="navbar">
       <div className="navbar__links">
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/posts">Posts</NavLink>
+        <NavLink
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          to="/about"
+        >
+          About
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          to="/posts"
+        >
+          Posts
+        </NavLink>
       </div>
     </div>
   );
