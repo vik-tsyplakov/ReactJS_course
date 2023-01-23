d2e92c218dc5311a09d5e58579b4ffb0c4daa567
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2023-01-23 13:17:22 +0400

    installed-React-Hover-Image

--- course/src/components/UI/footer/Footer.jsx
+++ course/src/components/UI/footer/Footer.jsx
@@ -3,6 +3,9 @@ import cl from "./Footer.module.css";
 import github_icon from "./../../../img/github_icon.svg";
 import linkedin_icon from "./../../../img/linkedin_icon.svg";
 import mail_icon from "./../../../img/mail_icon.svg";
+import github_icon_hover from "./../../../img/github_icon_hover.svg";
+import linkedin_icon_hover from "./../../../img/linkedin_icon_hover.svg";
+import mail_icon_hover from "./../../../img/mail_icon_hover.svg";
 
 export default function Footer() {
   return (
@@ -14,7 +17,12 @@ export default function Footer() {
               href="https://github.com/vik-tsyplakov/ReactJS_course/tree/course-continuation"
               target="_blank"
             >
-              <img className={cl.social__item} src={github_icon} alt="github" />
+              <img className={cl.item__img} src={github_icon} alt="github" />
+              <img
+                className={cl.item__img_hover}
+                src={github_icon_hover}
+                alt="github"
+              />
             </a>
           </div>
           <div className={cl.social__item}>
@@ -23,15 +31,25 @@ export default function Footer() {
               target="_blank"
             >
               <img
-                className={cl.social__item}
+                className={cl.item__img}
                 src={linkedin_icon}
+                alt="linkedin"
+              />
+              <img
+                className={cl.item__img_hover}
+                src={linkedin_icon_hover}
                 alt="linkedin"
               />
             </a>
           </div>
           <div className={cl.social__item}>
             <a href="mailto:vik.tsyplakov@gmail.com" target="_blank">
-              <img src={mail_icon} alt="mail" />
+              <img className={cl.item__img} src={mail_icon} alt="mail" />
+              <img
+                className={cl.item__img_hover}
+                src={mail_icon_hover}
+                alt="mail"
+              />
             </a>
           </div>
         </div>
