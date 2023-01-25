0923521a3976abd103b57984b67db0cbb852aaba
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2023-01-25 22:45:55 +0400

    created-css-module-ErrorPage

--- course/src/pages/ErrorPage.jsx
+++ course/src/pages/ErrorPage.jsx
@@ -1,17 +1,23 @@
 import React from "react";
+import cl from "./ErrorPage.module.css";
+import error_404 from "./../img/error_404.png";
 
 export default function ErrorPage() {
   return (
-    <main
-      style={{
-        color: "red",
-        fontWeight: "600",
-        textAlign: "center",
-        minHeight: "80vh",
-      }}
-    >
-      <p style={{ marginTop: "20px" }}>Error - invalid page address</p>
-      <p>Please enter a valid address or use navigation</p>
+    <main className={cl.errorPage__wrapper}>
+      <div className={cl.errorPage__content}>
+        <div className={cl.errorPage__text}>
+          <p>Error - invalid page address</p>
+          <p>Please enter a valid address or use the navigation</p>
+        </div>
+        <div>
+          <img
+            className={cl.errorPage__img}
+            src={error_404}
+            alt="Error - invalid page address"
+          />
+        </div>
+      </div>
     </main>
   );
 }
